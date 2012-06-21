33e5b6c6cf4f870ae44b865010a4a0156df23ab7
Damian Dunajski <damdun@gmail.com>
2012-06-22 00:40:31 +0200

    Initialization of project 'java-parameter-passing'

---
+++ java-parameter-passing/src/test/java/pl/geeksoft/examples/ParametersPassingTest.java
@@ -0,0 +1,50 @@
+package pl.geeksoft.examples;
+
+import static java.lang.System.identityHashCode;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ParametersPassingTest {
+
+	@Test
+	public void testSwapPrimitives() {
+		int a, b;
+		swap(a = 10, b = 20);
+		printPrimitiveValues("Outside swap", a, b);
+		// Check values
+		assertEquals(10, a);
+		assertEquals(20, b);
+	}
+
+	@Test
+	public void testSwapObjects() {
+		String a, b;
+		swap(a = "foo", b = "bar");
+		printObjectHashCodes("Outside swap", a, b);
+		// Check values
+		assertEquals("foo", a);
+		assertEquals("bar", b);
+	}
+
+	private void swap(int a, int b) {
+		printPrimitiveValues("Before swap", a, b);
+		int t = a; a = b; b = t;
+		printPrimitiveValues("After swap", a, b);
+	}
+
+	private void swap(String a, String b) {
+		printObjectHashCodes("Before swap", a, b);
+		String t = a; a = b; b = t;
+		printObjectHashCodes("After swap", a, b);
+	}
+
+	private void printPrimitiveValues(String message, int a, int b) {
+		System.out.println(message + " [a = " + a + "; b = " + b + "]");
+	}
+
+	private void printObjectHashCodes(String message, Object a, Object b) {
+		System.out.println(message + " [a.hashCode = " + identityHashCode(a) + "; b.hashCode = " + identityHashCode(b) + "]");
+	}
+
+}
